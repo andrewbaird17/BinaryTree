f63fd514c696ebd91f32b4b461393fdf6425a9f9
Andrew Baird <baird.andrew4@gmail.com>
2020-01-30 13:27:07 -0600

    Base Add Method of BST established. Node class with base variables set.

---
+++ BinarySearchTree/BinSearchTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinSearchTree
+    {
+        //Member Variables (HAS A)
+        Node start;
+        
+        //Constructor
+        public BinSearchTree()
+        {
+            start = null;
+        }
+
+        //Member Methods (CAN DO)
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (start == null)
+            {
+                start = node;
+                
+            }
+            else
+            {
+                Node current = start;
+                while (current.link != null)
+                {
+                    current = current.link;
+                }
+                current.link = node;
+            }
+        }
+
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        //Member Variables (HAS A)
+        public int data;
+        public Node leftChild;
+        public Node rightChild;
+        public Node link;
+
+        //Constructor
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+        //Member Methods (CAN DO)
+
+
+    }
+}
